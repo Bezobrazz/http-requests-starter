a911b38614211beebfa7dc313bb7fdc5d217b2da
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2023-02-13 15:21:14 +0200

    Try / catch added

--- src/App.js
+++ src/App.js
@@ -21,15 +21,25 @@ function App() {
 
   const [jokes, setJokes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchJokesHandler() {
     setIsLoading(true);
-    const response = await fetch(
-      "https://official-joke-api.appspot.com/jokes/programming/ten"
-    );
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://official-joke-api.appspot.com/jokes/programming/te"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await response.json();
 
-    const data = await response.json();
-    setJokes(data);
+      setJokes(data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   }
 
@@ -40,8 +50,14 @@ function App() {
       </section>
       <section>
         {!isLoading && jokes.length > 0 && <JokeList jokes={jokes} />}
-        {!isLoading && jokes.length === 0 && <p>No jokes found.</p>}
+        {!isLoading && jokes.length === 0 && !error && (
+          <p>
+            Press the button <span>☝️</span>
+          </p>
+        )}
+        {error && <p>No jokes found.</p>}
         {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
